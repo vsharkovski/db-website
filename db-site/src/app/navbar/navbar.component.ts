64c08c2170906cf236084bb418f9f835e247e3ad
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-10-10 16:35:50 +0400

    collapse navbar on routing

--- db-site/src/app/navbar/navbar.component.ts
+++ db-site/src/app/navbar/navbar.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'dbw-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
   isNavbarCollapsed: boolean = true;
+  routerSubscription?: Subscription;
 
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Collapse navbar (if vertical) every time navigation happens
+    this.routerSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isNavbarCollapsed = true;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSubscription?.unsubscribe();
+  }
 }
