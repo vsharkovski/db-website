ef71ce5af8eae29c2681b9a7372738133ac15d0b
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-10-25 19:01:24 +0400

    add missing files of api init

---
+++ db-api/src/main/kotlin/com/vsharkovski/dbpaperapi/DbpaperapiApplication.kt
@@ -0,0 +1,11 @@
+package com.vsharkovski.dbpaperapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DbpaperapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DbpaperapiApplication>(*args)
+}
---
+++ db-api/src/test/kotlin/com/vsharkovski/dbpaperapi/DbpaperapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vsharkovski.dbpaperapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DbpaperapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
