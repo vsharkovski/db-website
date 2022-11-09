cc06737a1a1797c51d0cedf6d6738c922cde04bd
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-11-09 13:03:12 +0400

    fix parsing mistake

--- db-api/src/main/kotlin/com/vsharkovski/dbpaperapi/service/SearchService.kt
+++ db-api/src/main/kotlin/com/vsharkovski/dbpaperapi/service/SearchService.kt
@@ -19,8 +19,8 @@ class SearchService(
             builder.with(
                 matcher.group(1),
                 matcher.group(2),
-                matcher.group(3),
                 matcher.group(4),
+                matcher.group(3),
                 matcher.group(5)
             )
         }
