da603425f5775dabae134611580def6efb6c3fa0
Vladimir Sharkovski <vsarkovskii@gmail.com>
2022-11-12 16:30:48 +0400

    improve page titles

--- db-site/src/app/app-routing.module.ts
+++ db-site/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { HighlightsPageComponent } from './highlights-page/highlights-page.component';
 import { FiguresPageComponent } from './figures-page/figures-page.component';
 import { MainPageComponent } from './main-page/main-page.component';
@@ -18,25 +17,22 @@ const routes: Routes = [
   {
     path: 'search',
     component: SearchAppComponent,
-    title:
-      'Search - A cross-verified database of notable people, 3500BC-2018AD',
+    title: 'Search - Notable people database',
   },
   {
     path: 'highlights',
     component: HighlightsPageComponent,
-    title:
-      'Highlights - A cross-verified database of notable people, 3500BC-2018AD',
+    title: 'Highlights - Notable people database',
   },
   {
     path: 'figures',
     component: FiguresPageComponent,
-    title:
-      'Figures - A cross-verified database of notable people, 3500BC-2018AD',
+    title: 'Figures - Notable people database',
   },
   {
     path: 'about',
     component: AboutPageComponent,
-    title: 'About - A cross-verified database of notable people, 3500BC-2018AD',
+    title: 'About - Notable people database',
   },
   {
     path: 'not-found',
