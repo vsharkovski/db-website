d17f60683f664b87dd89cb4e618f0ef87e7e2611
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-01-01 15:54:01 +0100

    make search wait status good

--- db-site/src/app/search-app/search-app.component.ts
+++ db-site/src/app/search-app/search-app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import {
   debounceTime,
   distinctUntilChanged,
@@ -8,6 +8,7 @@ import {
   merge,
   Subject,
   switchMap,
+  tap,
 } from 'rxjs';
 import { SearchQuery } from '../search-query.model';
 import { SearchResponse } from '../search-response.model';
@@ -27,6 +28,7 @@ export class SearchAppComponent implements OnInit {
 
   searchQueryChanged = new Subject<SearchQuery>();
   searchOptionsSubmitted = new Subject<void>();
+  pageOrSortChanged = new Subject<void>();
 
   hasAskedForResults: boolean = false;
   waitingForResults: boolean = false;
@@ -38,7 +40,10 @@ export class SearchAppComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Pass initial params from route to search options
+    // Pass initial params from route to search options.
+    // Whenever the requestedQuery object changes, the search-options element
+    // (which is bound to the object) will process the changes, and then
+    // emit an event which triggers searchQueryChanged.
     const initParams = this.route.snapshot.queryParams;
     this.requestedQuery = {
       term: initParams['term'] ?? '',
@@ -46,7 +51,8 @@ export class SearchAppComponent implements OnInit {
       sort: null,
     };
 
-    // Whenever the search options are changed, update the route
+    // Whenever the search query is changed, either from the search-options element
+    // or from changing a page or sort state, update the route with the new query.
     this.searchQueryChanged.subscribe((query: SearchQuery) => {
       this.latestQuery = this.mergeQueries(query, this.latestQuery);
       this.router.navigate([], {
@@ -60,9 +66,11 @@ export class SearchAppComponent implements OnInit {
       });
     });
 
-    // Whenever the route is updated or the search form is submitted,
-    // search for it.
     const resultsReceived = new Subject<void>();
+
+    // Whenever the route is updated or the search form is submitted,
+    // filter unnecessary options using debounceTime, distinctUntilChanged,
+    // and by making sure the search query has a 'term'.
     const readyToAskForResults = merge(
       this.searchOptionsSubmitted.pipe(
         map(() => this.route.snapshot.queryParams)
@@ -76,6 +84,8 @@ export class SearchAppComponent implements OnInit {
       distinctUntilChanged((a, b) => JSON.stringify(a) == JSON.stringify(b)),
       filter((params) => params['term'])
     );
+
+    // Ask the API for results.
     const results$ = readyToAskForResults.pipe(
       switchMap((params) =>
         this.searchService.getSearchResults(
@@ -86,59 +96,69 @@ export class SearchAppComponent implements OnInit {
         )
       )
     );
+
+    // Subscribe to the observable described above. Whenever results are received,
+    // update this.results to trigger binding events, and emit from resultsReceived
+    // to update the wait status for UI effects.
     results$.subscribe((results) => {
       this.results = results;
-      this.router.navigate([], {
-        relativeTo: this.route,
-        queryParams: this.route.snapshot.queryParams,
-      });
       resultsReceived.next();
     });
 
-    // Whenever the route is changed or the search results are received,
-    // update the wait status.
+    /*
+      Update the wait status in the following moments:
+      - About to ask the API for results (set to true)
+      - Just received results from the API (set to false)
+      - User changed the page or sort state (set to true in order to disable appropriate UI)
+    */
     merge(
-      readyToAskForResults,
-      resultsReceived.pipe(map(() => this.route.snapshot.params))
-    )
-      .pipe(map((params) => (params['term'] ? true : false)))
-      .subscribe((newWaitStatus) => {
-        this.waitingForResults = newWaitStatus;
-        if (newWaitStatus) {
-          this.hasAskedForResults = true;
-        }
-      });
+      readyToAskForResults.pipe(map(() => true)),
+      resultsReceived.pipe(map(() => false)),
+      this.pageOrSortChanged.pipe(map(() => true))
+    ).subscribe((newWaitStatus) => {
+      console.log('Got wait status', newWaitStatus);
+      this.waitingForResults = newWaitStatus;
+      if (newWaitStatus) {
+        this.hasAskedForResults = true;
+      }
+    });
   }
 
   onPageButtonClick(pageChange: number): void {
+    // Whenever the page is changed and it is possible to move to the new page,
+    // update the requestedQuery object which will trigger the mechanism
+    // as described in ngOnInit.
     if (
       ((pageChange === -1 && this.results?.hasPreviousPage) ||
         (pageChange === 1 && this.results?.hasNextPage)) &&
       this.latestQuery &&
       this.latestQuery.page + pageChange >= 0
     ) {
-      // Update the search options component with the new query
-      // This will afterwards be captured in the searchQueryChanged event
       this.requestedQuery = {
         term: this.latestQuery.term,
         page: this.latestQuery.page + pageChange,
         sort: this.latestQuery.sort,
       };
+      this.pageOrSortChanged.next();
     }
   }
 
   onSortStateChanged(sortState: SortState): void {
+    // Whenever the sort state is changed, emit from searchQueryChanged.
     if (this.latestQuery) {
-      // Fire the searchQueryChanged event with the new query
       this.searchQueryChanged.next({
         term: this.latestQuery.term,
         page: this.latestQuery.page,
         sort: sortState,
       });
+      this.pageOrSortChanged.next();
     }
   }
 
   mergeQueries(newQuery: SearchQuery, oldQuery?: SearchQuery): SearchQuery {
+    // Merge two query objects such that the non-essential parameters
+    // (sort state) are used from the old query if the new query does
+    // not specify them.
     return {
       term: newQuery.term,
       page: newQuery.page,
