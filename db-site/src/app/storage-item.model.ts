229dbba54bffa999549be74de6d67c0d6b97a87f
Vladimir Sharkovski <vsarkovskii@gmail.com>
2023-06-05 11:22:55 +0200

    cache responses

---
+++ db-site/src/app/storage-item.model.ts
@@ -0,0 +1,4 @@
+export interface StorageItem {
+  updatedMs: number;
+  value: any;
+}
--- db-site/src/app/timeline.service.ts
+++ db-site/src/app/timeline.service.ts
@@ -6,19 +6,10 @@ import { TimelinePoint } from './timeline-point.model';
 import { TimelineResponse } from './timeline-response.model';
 import { TimelineOptions } from './timeline-options.model';
 import { TimelineTimeStatistics } from './timeline-statistics.model';
+import { StorageItem } from './storage-item.model';
 
-const MAX_NUM_KEYS = 4;
-const TIMELINE_NUM_KEYS_KEY = 'timeline-keys';
-const TIMELINE_PART_KEY_BASE = 'timeline-data';
-
-// Standard Normal variate using Box-Muller transform.
-function gaussianRandom(mean = 0, stdev = 1) {
-  const u = 1 - Math.random(); // Converting [0,1) to (0,1]
-  const v = Math.random();
-  const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-  // Transform to the desired mean and standard deviation:
-  return z * stdev + mean;
-}
+const TIMELINE_RESPONSE_KEY = 'timeline-data';
+const STORAGE_LIFETIME_MS = 1000 * 60 * 60 * 24; // One day.
 
 @Injectable({
   providedIn: 'root',
@@ -29,6 +20,15 @@ export class TimelineService {
   requestedDataFromApi = false;
 
   constructor(private http: HttpClient, private errorService: ErrorService) {
+    // Standard Normal variate using Box-Muller transform.
+    function gaussianRandom(mean = 0, stdev = 1) {
+      const u = 1 - Math.random(); // Converting [0,1) to (0,1]
+      const v = Math.random();
+      const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+      // Transform to the desired mean and standard deviation:
+      return z * stdev + mean;
+    }
+
     // for (let i = 0; i < 100000; i++) {
     //   const b = Math.round(gaussianRandom(1600, 100));
     //   const ni = Math.round(30 + Math.random() * 10);
@@ -61,7 +61,7 @@ export class TimelineService {
           )
         )
         .subscribe((response) => {
-          // this.cacheResponse(response);
+          this.cacheResponse(response);
 
           const data = this.processResponse(response);
           this.timelineData = data;
@@ -90,46 +90,22 @@ export class TimelineService {
    * @returns Whether caching was successful.
    */
   private cacheResponse(response: TimelineResponse): boolean {
-    // Remove all cached keys from possible previous attempts.
-    for (let keyNumber = 0; keyNumber < MAX_NUM_KEYS; keyNumber++) {
-      const key = this.getKeyFromKeyNumber(keyNumber);
-      window.localStorage.removeItem(key);
+    const toStore = JSON.stringify({
+      updatedMs: new Date().getTime(),
+      value: response,
+    } as StorageItem);
+
+    window.localStorage.removeItem(TIMELINE_RESPONSE_KEY);
+
+    try {
+      window.localStorage.setItem(TIMELINE_RESPONSE_KEY, toStore);
+    } catch (e) {
+      // console.log(
+      //   `Could not cache response in local storage. Length: ${toStore.length}.`,
+      //   e
+      // );
+      return false;
     }
-
-    const responseString = JSON.stringify(response);
-    const numKeys = Math.min(responseString.length, MAX_NUM_KEYS);
-    const partLength = Math.ceil(responseString.length / numKeys);
-
-    let numKeysUsed = 0;
-    let startPos = 0;
-
-    // Split response into parts and cache each part.
-    for (let keyNumber = 0; keyNumber < numKeys; keyNumber++) {
-      if (startPos >= responseString.length) {
-        break;
-      }
-
-      const length = Math.min(partLength, responseString.length - startPos);
-      const part = responseString.substring(startPos, length);
-      const key = this.getKeyFromKeyNumber(keyNumber);
-
-      try {
-        window.localStorage.setItem(key, part);
-
-        numKeysUsed++;
-        startPos += length;
-      } catch (e) {
-        console.log(
-          `Could not cache response part ${keyNumber} in local storage. Full length: ${responseString.length}. Part length: ${length}`,
-          e
-        );
-        return false;
-      }
-    }
-
-    // Cache number of keys used.
-    window.localStorage.removeItem(TIMELINE_NUM_KEYS_KEY);
-    window.localStorage.setItem(TIMELINE_NUM_KEYS_KEY, numKeysUsed.toString());
 
     return true;
   }
@@ -138,46 +114,31 @@ export class TimelineService {
    * @returns Whether it was loaded.
    */
   private loadDataFromCache(): boolean {
-    return false;
-
-    const numKeysString = window.localStorage.getItem(TIMELINE_NUM_KEYS_KEY);
-    if (numKeysString === null) {
-      console.log('Could not load number of keys from cache.');
+    const storedString = window.localStorage.getItem(TIMELINE_RESPONSE_KEY);
+    if (storedString === null) {
+      // console.log('Nothing was cached.');
       return false;
     }
 
-    const numKeys = Number(numKeysString);
-    const parts = [];
-
-    for (let keyNumber = 0; keyNumber < numKeys; keyNumber++) {
-      const key = this.getKeyFromKeyNumber(keyNumber);
-      const part = window.localStorage.getItem(key);
-
-      if (part === null) {
-        console.log(
-          `Could not find part ${keyNumber} of ${numKeys} from cache.`
-        );
-        return false;
-      } else {
-        parts.push(part);
-      }
-    }
-
+    let storageItem!: StorageItem;
     try {
-      const fullResponse = JSON.parse(parts.join(''));
-      const data = this.processResponse(fullResponse);
-
-      this.timelineData = data;
-      console.log('Loading from cache successful.');
-      return true;
+      storageItem = JSON.parse(storedString);
     } catch (e) {
-      console.log('Loading from cache failed.', e);
+      // console.log('Parsing cached data failed.', e);
       return false;
     }
-  }
 
-  private getKeyFromKeyNumber(keyNumber: number): string {
-    return `${TIMELINE_PART_KEY_BASE}-${keyNumber}`;
+    const currentTimeMs = new Date().getTime();
+    if (storageItem.updatedMs + STORAGE_LIFETIME_MS < currentTimeMs) {
+      // console.log('Cached data was expired.');
+      return false;
+    }
+
+    const data = this.processResponse(storageItem.value);
+    this.timelineData = data;
+
+    // console.log('Loading cached data successful.');
+    return true;
   }
 
   /**
